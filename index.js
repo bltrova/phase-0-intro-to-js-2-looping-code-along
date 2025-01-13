8db611960c48c44ee5753ed2cea0fb3c5b38b2ca
Bruno Trova <bltrova@gmail.com>
2025-01-13 09:54:32 -0800

    looping lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise";
+console.log(writeCards(names, event));
+console.log(countDown(10));
+console.log(countDown(4));
+
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+        console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);  
+    }
+    return messages;
+}
+
+function countDown(numbers) {
+    for (let i = numbers; i >= 0; i--) {
+        console.log(i);
+    }
+}
+countDown(10);
+countDown(4);
